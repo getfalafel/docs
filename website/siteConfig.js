793214e7a730dff1bda41f601971bb3c9ad9d277
mateusrovedaa <mateus_roveda@hotmail.com>
2021-01-28 21:34:09 -0300

    Change name

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,7 +21,7 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Farafel', // Title for your website.
+  title: 'Falafel', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://your-docusaurus-test-site.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
